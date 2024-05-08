022f441c8549306ec255ee90a42c7b7909a6ab4e
Mingfei Li <ml@mingfei.io>
2024-05-08 09:36:14 -0700

    Update config.py

--- config.py
+++ config.py
@@ -33,4 +33,4 @@ class AtariPongConfig(Config):
     buffer_size = 1_000_000
     test_freq = 10
     model = "conv_net"
-    exp_id = "exp-25:atari-pong,buf_size=1m,batch_size=64,n_train=5k,adam"
+    exp_id = "exp-26:atari-pong,buf_size=1m,batch_size=64,n_train=5k,adam"
