0d9f8dd01e25866c1027d27e7a9034a224263a1b
Mingfei Li <ml@mingfei.io>
2024-05-09 19:11:52 -0700

    easy-pong:exp-4:buf_size=1.5m,batch_size=96,learning_start=100k,lr=1e-4->1e-5,n_train=30k,n_eps=1m,adam
        num_episodes_train = 30_000
        max_eps = 1
        min_eps = 0.1
        n_eps = 1_000_000
        max_lr = 1e-4
        min_lr = 1e-5
        n_lr = 2_500_000
        target_update_freq = 10_000
        batch_size = 96
        gamma = 0.99
        learning_start = 100_000
        buffer_size = 1_500_000
        test_freq = 10
        model = "conv_net"

--- config.py
+++ config.py
@@ -36,18 +36,18 @@ class AtariPongConfig(Config):
     exp_id = "exp-42:atari-pong,buf_size=1.5m,batch_size=96,learning_start=50k,lr=1e-4,n_train=5k,n_eps=2m,adam"
 
 class EasyPongConfig(Config):
-    num_episodes_train = 25_000
+    num_episodes_train = 30_000
     max_eps = 1
     min_eps = 0.1
     n_eps = 1_000_000
     max_lr = 1e-4
-    min_lr = 1e-4
+    min_lr = 1e-5
     n_lr = 2_500_000
     target_update_freq = 10_000
     batch_size = 96
     gamma = 0.99
-    learning_start = 50_000
+    learning_start = 100_000
     buffer_size = 1_500_000
     test_freq = 10
     model = "conv_net"
-    exp_id = "easy-pong:exp-3:buf_size=1.5m,batch_size=96,learning_start=50k,lr=5e-4,n_train=25k,n_eps=1m,adam"
+    exp_id = "easy-pong:exp-4:buf_size=1.5m,batch_size=96,learning_start=100k,lr=1e-4->1e-5,n_train=25k,n_eps=1m,adam"
