252c00d7caa64ed71947b55aece8ff9d69c65f4c
Mingfei Li <ml@mingfei.io>
2024-05-09 23:51:11 -0700

    easy-pong:exp-9:buf_size=1.5m,batch_size=96,learning_start=100k,lr=5e-6->5e-7,n_train=25k,n_eps=1m,clip_val=10,adam
        num_episodes_train = 30_000
        max_eps = 1
        min_eps = 0.1
        n_eps = 1_000_000
        max_lr = 5e-6
        min_lr = 5e-7
        n_lr = 2_500_000
        target_update_freq = 10_000
        batch_size = 96
        gamma = 0.99
        learning_start = 100_000
        buffer_size = 1_500_000
        test_freq = 10
        clip_val = 10
        betas = (0.9, 0.999)
        model = "conv_net"

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ class EasyPongConfig(Config):
     max_eps = 1
     min_eps = 0.1
     n_eps = 1_000_000
-    max_lr = 1e-5
-    min_lr = 1e-6
+    max_lr = 5e-6
+    min_lr = 5e-7
     n_lr = 2_500_000
     target_update_freq = 10_000
     batch_size = 96
@@ -56,4 +56,4 @@ class EasyPongConfig(Config):
     clip_val = 10
     betas = (0.9, 0.999)
     model = "conv_net"
-    exp_id = "easy-pong:exp-8:buf_size=1.5m,batch_size=96,learning_start=100k,lr=1e-5->1e-6,n_train=25k,n_eps=1m,clip_val=10,adam"
+    exp_id = "easy-pong:exp-9:buf_size=1.5m,batch_size=96,learning_start=100k,lr=5e-6->5e-7,n_train=25k,n_eps=1m,clip_val=10,adam"
