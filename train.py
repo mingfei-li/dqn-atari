18ff93120c6bcb0e73caeaadda5d23c1df6a28e0
Mingfei Li <ml@mingfei.io>
2024-05-21 17:17:28 -0700

    pong:exp-seed1234-1
        num_episodes_train = 8_000
        max_eps = 1
        min_eps = 0.1
        n_eps = 5_000_000
        max_lr = 7.5e-5
        min_lr = 7.5e-6
        n_lr = 1_500_000
        target_update_freq = 1_000
        batch_size = 96
        gamma = 0.99
        learning_start = 100_000
        buffer_size = 1_000_000
        eval_freq = 10
        episodic = False
        training_freq = 4
        model_save_freq = 1_000_000
        model = "cnn"
        exp_id = "pong:exp-seed1234-1"

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def cartpole():
         env.close()
 
 def pong():
-    run_id = 42
+    run_id = 1234
     set_seed(run_id)
     env = gym.make('PongNoFrameskip-v4', render_mode="rgb_array", obs_type="grayscale")
     env = PongWrapper(env)
