98f37b48d35c55eed31f2d70b37545c2ed013871
Mingfei Li <ml@mingfei.io>
2024-05-29 23:21:33 -0700

    Update logger.py
    Add max logging for episode stats

--- utils/logger.py
+++ utils/logger.py
@@ -4,20 +4,24 @@ from statistics import mean
 
 class Logger():
     def __init__(self, log_dir):
-        self.scalar_buffer = {}
+        self.step_stats = {}
+        self.episode_stats = {}
         self.writer = SummaryWriter(log_dir=f"{log_dir}")
 
     def add_step_stats(self, key, value):
-        if key not in self.scalar_buffer.keys():
-            self.scalar_buffer[key] = deque(maxlen=200)
-        self.scalar_buffer[key].append(value)
+        if key not in self.step_stats.keys():
+            self.step_stats[key] = deque(maxlen=200)
+        self.step_stats[key].append(value)
 
     def add_episode_stats(self, key, value):
-        if key not in self.scalar_buffer.keys():
-            self.scalar_buffer[key] = deque(maxlen=50)
-        self.scalar_buffer[key].append(value)
+        if key not in self.episode_stats.keys():
+            self.episode_stats[key] = deque(maxlen=50)
+        self.episode_stats[key].append(value)
     
     def flush(self, t):
-        for key, value in self.scalar_buffer.items():
+        for key, value in self.step_stats.items():
             self.writer.add_scalar(key, mean(value), t)
+        for key, value in self.episode_stats.items():
+            self.writer.add_scalar(f"{key}.avg", mean(value), t)
+            self.writer.add_scalar(f"{key}.max", max(value), t)
         self.writer.flush()
