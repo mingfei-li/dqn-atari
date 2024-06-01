5ae9ec0fb3177cb740f589fe2bf098dc7af9df19
Mingfei Li <ml@mingfei.io>
2024-06-01 14:22:53 -0700

    breakout exp-15
        n_steps_train = 15_000_000
        max_eps = 1
        min_eps = 0.1
        n_eps = 1_000_000
        initial_lr = 7.5e-5
        lr_half_life = 1_000_000
        target_update_freq = 1_000
        batch_size = 96
        gamma = 0.99
        learning_start = 100_000
        buffer_size = 1_000_000
        eval_freq = 10_000
        episodic = True
        training_freq = 4
        model_save_freq = 1_000_000
        game = "breakout"
        exp_id = "exp-15"

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config():
     max_eps = 1
     min_eps = 0.1
     n_eps = 1_000_000
-    initial_lr = 2.5e-4
-    lr_half_life = 3_000_000
+    initial_lr = 7.5e-5
+    lr_half_life = 1_000_000
     target_update_freq = 1_000
     batch_size = 96
     gamma = 0.99
@@ -15,4 +15,4 @@ class Config():
     training_freq = 4
     model_save_freq = 1_000_000
     game = "breakout"
-    exp_id = "exp-14"
+    exp_id = "exp-15"
