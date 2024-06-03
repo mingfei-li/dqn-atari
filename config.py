b831a18eedb027eafde6e6c35971f7448b9490f1
Mingfei Li <ml@mingfei.io>
2024-06-03 10:49:00 -0700

    breakout exp-22
    count lost of life as done
    
        n_steps_train = 15_000_000
        max_eps = 1
        min_eps = 0.1
        n_eps = 1_000_000
        initial_lr = 7.5e-5
        lr_half_life = 1_000_000
        target_update_freq = 1_000
        batch_size = 96
        gamma = 0.99
        learning_start = 100_000
        buffer_size = 1_000_000
        eval_freq = 10_000
        episodic = True
        training_freq = 4
        model_save_freq = 1_000_000
        game = "breakout"
        exp_id = "exp-22"

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 class Config():
     n_steps_train = 15_000_000
     max_eps = 1
-    min_eps = 0.01
+    min_eps = 0.1
     n_eps = 1_000_000
     initial_lr = 7.5e-5
     lr_half_life = 1_000_000
     target_update_freq = 1_000
     batch_size = 96
-    gamma = 0.999
+    gamma = 0.99
     learning_start = 100_000
-    buffer_size = 3_000_000
+    buffer_size = 1_000_000
     eval_freq = 10_000
     episodic = True
     training_freq = 4
     model_save_freq = 1_000_000
     game = "breakout"
-    exp_id = "exp-21"
+    exp_id = "exp-22"
