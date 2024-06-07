64c5d9bc6838a5af73f5f3a9d97b9eabb687bb53
Mingfei Li <ml@mingfei.io>
2024-06-06 23:56:26 -0700

    breakout exp-46

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ class Config():
     n_steps_train = 50_000_000
     max_eps = 1
     min_eps = 0.01
-    n_eps = 5_000_000
+    n_eps = 2_000_000
     eps_schedule = 'exponential'
     max_lr = 7.5e-5
     min_lr = 7.5e-6
-    n_lr = 5_000_000
-    lr_schedule = 'linear'
-    target_update_freq = 10_000
+    n_lr = 10_000_000
+    lr_schedule = 'exponential'
+    target_update_freq = 40_000
     batch_size = 96
     gamma = 0.99
     learning_start = 100_000
@@ -18,4 +18,4 @@ class Config():
     training_freq = 4
     model_save_freq = 1_000_000
     game = "breakout"
-    exp_id = "exp-45"
+    exp_id = "exp-46"
